fea914bbe1f44ffd3960b34ef469772f8de58e57
subham <subhamsarangi23@gmail.com>
2025-05-24 01:48:52 +0530

    Create a js array of object contains skill data

---
+++ src/pages/AboutPage/progressBar.js
@@ -0,0 +1,32 @@
+export const skillsProgress = [
+  {
+    skill: "ReactJs",
+    about: "I'm Suvam Sarangi, a recent Software Engineering graduate based in Hyderabad",
+    progress: 75,
+  },
+  {
+    skill: "JavaScript",
+    about: "I'm Suvam Sarangi, a recent Software Engineering graduate based in Hyderabad",
+    progress: 65,
+  },
+  {
+    skill: "Java",
+    about: "I'm Suvam Sarangi, a recent Software Engineering graduate based in Hyderabad",
+    progress: 90,
+  },
+  {
+    skill: "HTML, CSS",
+    about: "I'm Suvam Sarangi, a recent Software Engineering graduate based in Hyderabad",
+    progress: 95,
+  },
+  {
+    skill: "Tailwind",
+    about: "I'm Suvam Sarangi, a recent Software Engineering graduate based in Hyderabad",
+    progress: 75,
+  },
+  {
+    skill: "Spring",
+    about: "I'm Suvam Sarangi, a recent Software Engineering graduate based in Hyderabad",
+    progress: 80,
+  },
+];
