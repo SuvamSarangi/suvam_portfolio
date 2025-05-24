5f923899457d96565662a0c3362f2094d173c225
subham <subhamsarangi23@gmail.com>
2025-05-25 01:45:22 +0530

    Change grid responsive to Custom grid-col according to screen and add extra skill and their Logo

--- src/pages/AboutPage/SkillProgressBar.jsx
+++ src/pages/AboutPage/SkillProgressBar.jsx
@@ -18,14 +18,15 @@ const SkillProgressBar = ({ skills }) => {
   }, [isInView, skills]);
 
   return (
-    <div className="w-full min-h-screen px-4 sm:px-6 md:px-12 py-12">
+    <div className="w-full min-h-screen px-4 sm:px-6 md:px-12 py-12 border border-amber-100">
       <h1 className="text-3xl sm:text-4xl font-bold text-center mb-12 text-white">
         My Skills
       </h1>
 
+          {/* MySkill cards */}
       <div
-        className="grid gap-7 w-full"
-        style={{ gridTemplateColumns: "repeat(auto-fit, minmax(380px, 1fr))" }}
+        className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-full  gap-7"
+        // style={{ gridTemplateColumns: "repeat(auto-fit, minmax(250px, 1fr))" }}
         ref={scrollRef}
       >
         {skills.map((prog, index) => (
@@ -33,7 +34,8 @@ const SkillProgressBar = ({ skills }) => {
             key={index}
             className="h-48 bg-white p-4 flex flex-col justify-evenly rounded-xl shadow"
           >
-            <h2 className="text-lg font-semibold text-gray-800">
+          <img  src={prog.image} className="w-12 h-12"/>
+            <h2 className="text-lg font-semibold text-gray-800 text-right">
               {prog.skill}
             </h2>
             <p className="text-sm text-gray-600">{prog.about}</p>
