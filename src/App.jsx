7253fe3d3b905a44596b6ae611d2f821bcfe5065
subham <subhamsarangi23@gmail.com>
2025-08-03 03:03:01 +0530

    change component Skill sectiom component name and render in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import { Link, Element } from 'react-scroll';
 import Navigation from './components/Navigation/Navigation.jsx'
 import Home from './pages/HomePage/Home.jsx'
 import About from './pages/AboutPage/About.jsx'
-import Portfolio from './pages/Portfolio/Portfolio.jsx'
 import Contact from './pages/ContactPage/Contact.jsx'
+import Project from './pages/Projects/Project.jsx';
 
 function App() {
 
@@ -14,7 +14,7 @@ function App() {
        <Navigation />
       <Element name="home"><Home /></Element>
       <Element name="about"><About /></Element>
-      <Element name="portfolio"><Portfolio /></Element>
+      <Element name="project"><Project /></Element>
       <Element name="contact"><Contact /></Element>
      
     </div>
