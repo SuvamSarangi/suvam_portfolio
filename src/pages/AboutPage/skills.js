2276c0b350c85e8b6f7e070cb731a0896dbbbc27
subham <subhamsarangi23@gmail.com>
2025-08-03 02:46:10 +0530

    change file name and inside array of object data accourding to skills

---
+++ src/pages/AboutPage/skills.js
@@ -0,0 +1,157 @@
+export const frontend= [
+  {
+    image:[
+      {
+          src:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+          alt:"Html5"
+      }
+    ],
+    skill: "Html5",
+    
+  },
+  {
+       image:[
+      {
+          src:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+          alt:"Css"
+      }
+    ],
+    skill: "Css",
+   
+  },
+  {
+      image:[
+      {
+          src:"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-plain.svg",
+          alt:"Tailwind CSS"
+      },
+    ],
+      skill: "Tailwind",
+  },
+  {
+       image:[
+      {
+          src:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+          alt:"Javascript"
+      }
+    ],
+    skill: "JavaScript",
+   
+  },
+   {
+      image:[
+      {
+          src:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+          alt:"Reactjs"
+      },
+    ],
+      skill: "ReactJs",
+  },
+];
+
+export const backend= [
+ 
+  {
+      image:[
+      {
+          src:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg",
+          alt:"java"
+      }
+    ],
+    skill: "Java",
+  },
+  {
+       image:[
+      {
+          src:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original.svg",
+          alt:"springboot"
+      }
+    ],
+    skill: "Spring Boot",
+
+  },
+  {
+      image:[
+      {
+          src:"https://upload.wikimedia.org/wikipedia/commons/4/44/Spring_Security_Logo.png",
+          alt:"spring security"
+      }
+    ],
+    skill: "Spring Security",
+
+  },
+   {
+       image:[
+      {
+          src:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+          alt:"React"
+      }
+    ],
+    skill: "Oracle",
+    
+  },
+ 
+  {
+       image:[
+      {
+          src:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+          alt:"React"
+      }
+    ],
+    skill: "Git/Github",
+  
+  },
+];
+
+export const database= [
+ 
+   {
+       image:[
+      {
+          src:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/oracle/oracle-original.svg",
+          alt:"oracle"
+      }
+    ],
+    skill: "Oracle",
+
+  },
+  {
+       image:[
+      {
+          src:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+          alt:"React"
+      }
+    ],
+    skill: "Oracle",
+
+  },
+ 
+ 
+];
+
+export const tools= [
+ 
+  {
+     image:[
+      {
+          src:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+          alt:"Git"
+      }
+    ],
+    skill: "Git",
+  
+  },
+   {
+       image:[
+      {
+          src:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
+          alt:"github"
+      }
+    ],
+    skill: "GitHub",
+  
+  },
+ 
+];
+
+ 
