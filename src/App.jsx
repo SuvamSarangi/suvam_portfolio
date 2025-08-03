2bf68f7494b0754430d8ffe83dc2daec6699bb6d
subham <subhamsarangi23@gmail.com>
2025-08-04 02:58:25 +0530

    Add Skill component in App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Home from './pages/HomePage/Home.jsx'
 import About from './pages/AboutPage/About.jsx'
 import Contact from './pages/ContactPage/Contact.jsx'
 import Project from './pages/Projects/Project.jsx';
+import Education from './pages/EducationPage/Education.jsx';
+import Skill from './pages/AboutPage/Skill.jsx';
 
 function App() {
 
@@ -13,8 +15,10 @@ function App() {
     <div>
        <Navigation />
       <Element name="home"><Home /></Element>
-      <Element name="about"><About /></Element>
+      <Element name="about"><About/></Element>
+      <Element name="skill"><Skill/></Element>
       <Element name="project"><Project /></Element>
+      <Element name="education"><Education /></Element>
       <Element name="contact"><Contact /></Element>
      
     </div>
