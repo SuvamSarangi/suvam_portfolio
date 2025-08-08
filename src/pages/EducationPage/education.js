b0da1b20afeabc9ee7bf0ac25fdf286143b69ed1
subham <subhamsarangi23@gmail.com>
2025-08-09 02:27:33 +0530

    create a ecucation js file for handle data dynamically

---
+++ src/pages/EducationPage/education.js
@@ -0,0 +1,26 @@
+ export const educationData = [
+  {
+    title: "Master's Degree",
+    institution: "Centurion University",
+    field: "Computer Application",
+    year: "2022 - 2024",
+  },
+  {
+    title: "Bachelor's Degree",
+    institution: "Utkal University",
+    field: "Botany",
+    year: "2016 - 2019",
+  },
+  {
+    title: "Council Of Higher Secondary,Odisha",
+    institution: "Parimal Degree Mahavidyalaya",
+    field: "Science",
+    year: "2014 - 2016",
+  },
+  {
+    title: "Board Of Higer Secondary,Odisha",
+    institution: "Dandimal High School",
+    field: "General Education",
+    year: "2014",
+  },
+];
