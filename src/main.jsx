159b1dc0afda35c39bb9ee2191375146a81ef53c
subham <subhamsarangi23@gmail.com>
2025-08-19 23:48:45 +0530

    import fontawesome

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import React from 'react'
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
