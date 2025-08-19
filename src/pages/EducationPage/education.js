b11c03cc54a3703eb79b63af3925a16796218d33
subham <subhamsarangi23@gmail.com>
2025-08-19 23:22:21 +0530

    add logo in two education object

--- src/pages/EducationPage/education.js
+++ src/pages/EducationPage/education.js
@@ -1,23 +1,28 @@
+
  export const educationData = [
   {
-    title: "Master's Degree",
+    image:"fas fa-user-graduate" ,
+    title: "Master Of Computer Application",
     institution: "Centurion University",
     field: "Computer Application",
     year: "2022 - 2024",
   },
   {
-    title: "Bachelor's Degree",
+    image:"fas fa-chart-line",
+    title: "Bachelor of Science",
     institution: "Utkal University",
     field: "Botany",
     year: "2016 - 2019",
   },
   {
+    image:"fas fa-book-open",
     title: "Council Of Higher Secondary,Odisha",
     institution: "Parimal Degree Mahavidyalaya",
     field: "Science",
     year: "2014 - 2016",
   },
   {
+    image:"fa-solid fa-certificate",
     title: "Board Of Higer Secondary,Odisha",
     institution: "Dandimal High School",
     field: "General Education",
