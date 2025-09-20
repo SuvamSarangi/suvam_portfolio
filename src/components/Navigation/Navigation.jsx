52033cc5d4c50bd6fa80fc8b08eada3e93e1af76
subham <subhamsarangi23@gmail.com>
2025-09-21 01:24:19 +0530

    add llinkedin and github profile link

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,21 +3,21 @@ import { Link } from "react-scroll";
 import { navItems } from "./navItems.js";
 import MobileMenu from "./MobileMenu.jsx";
 const Navigation = () => {
-   const [isOpen, setIsOpen] = useState(false);   // LIFTED STATE
-       const toggleMenu = () => {
-      setIsOpen(!isOpen);
+  const [isOpen, setIsOpen] = useState(false); // LIFTED STATE
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
   };
 
   return (
-    <nav className="fixed top-0 left-0 w-full text-white flex justify-around items-center py-5 z-30
-    bg-[#0E0E10]/50 backdrop-blur-md  shadow-lg rounded-md">
+    <nav
+      className="fixed top-0 left-0 w-full text-white flex justify-around items-center py-5 z-30
+    bg-[#0E0E10]/50 backdrop-blur-md  shadow-lg rounded-md"
+    >
       <div>
-      {/* <img src="suvam.png" className="w-14 h-14 rounded-full "/> */}
+        {/* <img src="suvam.png" className="w-14 h-14 rounded-full "/> */}
         <h2 className=" font-bold text-white ">Suvam</h2>
-        
       </div>
-      <ul
-        className="md:flex space-x-10 text-base font-medium hidden" >
+      <ul className="md:flex space-x-10 text-base font-medium hidden">
         {navItems.map((item, index) => (
           <li key={index} className="relative group cursor-pointer font-serif">
             <Link
@@ -37,14 +37,29 @@ const Navigation = () => {
       </ul>
 
       <div className="hidden md:block ">
-       <div className="flex gap-5">
-       <img className="w-12 h-12 invert-100" src="/public/linkedin.png"/>
-       <img className="w-12 h-12" src="/public/git.png"/>
-       </div>
+        <div className="flex gap-5">
+          <a
+            href="https://www.linkedin.com/in/subham-sarangi/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img className="w-8 h-8 invert-100" src="public/linkedin.png" />
+          </a>
+          <a
+            href="https://github.com/SuvamSarangi?tab=repositories"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img className="w-8 h-8" src="public/git.png" />
+          </a>
+        </div>
       </div>
 
-     <MobileMenu isOpen={isOpen} setIsOpen={setIsOpen} toggleMenu={toggleMenu}/>
-    
+      <MobileMenu
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        toggleMenu={toggleMenu}
+      />
     </nav>
   );
 };
